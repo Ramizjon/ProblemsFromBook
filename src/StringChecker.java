9334969fcbf5196479b3e73a06e72ad7b63d197a
Ramiz Abadulla <abadulla@live.ru>
2015-11-17 16:22:25 +0200

    added comments

--- src/StringChecker.java
+++ src/StringChecker.java
@@ -1,9 +1,15 @@
 import java.util.HashSet;
 import java.util.Set;
 
+// TASK:
+// Implement an algorithm to determine if a string has all unique characters. 
+// What if you cannot use additional data structures?
+
 
 public class StringChecker {
 
+	//Solution with use of Set as an additional data structure  
+	//based on uniqueness of all Set elements.
 	public boolean checkAllUniqueChars1 (String inputString){
 		Set <Character> charSet = new HashSet <Character> ();
 		char [] charArray = inputString.toCharArray();
@@ -14,6 +20,8 @@ public class StringChecker {
 		return (charSet.size() == charArray.length);
 	}
 	
+	
+	//Solution without using any additional data structures
 	public boolean checkAllUniqueChars2 (String inputString){
 		char [] charArray = inputString.toCharArray();
 		for (char c: charArray){
