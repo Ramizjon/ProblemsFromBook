0fa46f6339bb2c2c1697c6c5167b08754c001dd4
Ramiz <abadulla@live.ru>
2015-11-24 16:35:39 +0200

    Corrected some issues

---
+++ Problem1/src/StringCheckerInterface.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**TASK:
+* Implement an algorithm to determine if a string has all unique characters. 
+* What if you cannot use additional data structures?
+*/
+
+public class StringChecker {
+
+	public boolean checkAllUniqueChars (String inputString);
+
+}
