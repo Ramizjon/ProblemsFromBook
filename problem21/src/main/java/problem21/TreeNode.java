d05e05365cb485e637ad90711f19af054303bd32
Ramiz <abadulla@live.ru>
2016-01-21 14:25:02 +0200

    solution of problem 21

---
+++ problem21/src/main/java/problem21/TreeNode.java
@@ -0,0 +1,37 @@
+package problem21;
+
+public class TreeNode {
+	private int data;
+	private TreeNode right;
+	private TreeNode left;
+	
+	public TreeNode(int data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public TreeNode getRight() {
+		return right;
+	}
+
+	public void setRight(TreeNode right) {
+		this.right = right;
+	}
+
+	public TreeNode getLeft() {
+		return left;
+	}
+
+	public void setLeft(TreeNode left) {
+		this.left = left;
+	}
+}
---
+++ problem21/src/main/java/problem21/TreeToList.java
@@ -0,0 +1,87 @@
+package problem21;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeToList {
+	
+	ArrayList al = new ArrayList ();
+	StringBuilder sb = new StringBuilder ();
+
+	public int height(TreeNode root){
+ 		if (root==null) return 0;
+ 		return 1 + Math.max(height(root.getLeft()),height(root.getRight()));
+ 	}
+	
+	public void printLevels(TreeNode root, int h){
+ 		if(root==null) return;
+ 		if(h==1) System.out.print(" " + root.getData());
+ 		else{
+ 			printLevels(root.getLeft(),h-1);
+ 			printLevels(root.getRight(),h-1);
+ 		}
+	}
+	
+	public void levelOrderQueue(TreeNode root){
+ 		Queue q = new LinkedList();
+ 		int levelNodes =0;
+ 		if(root==null) return;
+ 		q.add(root);
+ 		while(!q.isEmpty()){
+ 		      levelNodes = q.size();
+               	      ListNode head = null;
+        	      ListNode curr = null;
+     			while(levelNodes != 0){
+				TreeNode n = (TreeNode)q.remove();
+				ListNode ln = new ListNode(n.getData());
+				if(head==null){
+					head = ln;
+					curr = ln;
+				}else{
+					curr.setNext(ln);
+					curr = curr.getNext();
+				}
+				if(n.getLeft()!=null) q.add(n.getLeft());
+				if(n.getRight()!=null) q.add(n.getRight());
+				levelNodes--;
+			}
+			al.add(head);
+		}
+
+		printList(al);
+	}
+	
+	public void printList(ArrayList al){
+		Iterator it = al.iterator();
+		while(it.hasNext()){
+		  ListNode head = (ListNode) it.next();
+		  while(head!=null){ sb.append(head.getData());
+			  System.out.print(" " + head.getData());
+			  head = head.getNext();
+		  }
+		  sb.append("\n");
+		  System.out.println("");
+		}
+	}
+
+	public String toString() {
+		return sb.toString();
+	}
+	
+	public static void main (String[] args) throws java.lang.Exception {
+		TreeNode root = new TreeNode(2);
+		root.setLeft(new TreeNode(16));
+		root.setRight(new TreeNode(35));
+		root.getLeft().setLeft(new TreeNode(21));
+		root.getLeft().setRight(new TreeNode(85));
+		root.getRight().setLeft(new TreeNode(44));
+		root.getRight().setRight(new TreeNode(11));
+
+		TreeToList i  = new TreeToList();
+		//i.levelOrder(root);
+		i.levelOrderQueue(root);
+	}
+	
+}
